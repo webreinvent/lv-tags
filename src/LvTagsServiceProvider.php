74642089ee88b26d66fc885cf8c8bd738a9357e5
Pradeep Kumar <webreinvent@gmail.com>
2018-12-26 18:59:03 +0530

    restructured the package

--- src/LvTagsServiceProvider.php
+++ src/LvTagsServiceProvider.php
@@ -48,7 +48,7 @@ class LvTagsServiceProvider extends ServiceProvider {
 
     private function handleConfigs() {
 
-        $configPath = __DIR__ . '/../config/lvtags.php';
+        $configPath = __DIR__ . '/Config/lvtags.php';
 
         $this->publishes([$configPath => config_path('lvtags.php')]);
 
@@ -57,23 +57,25 @@ class LvTagsServiceProvider extends ServiceProvider {
 
     private function handleTranslations() {
 
-        $this->loadTranslationsFrom(__DIR__.'/../lang', 'lvtags');
+        $this->loadTranslationsFrom(__DIR__.'/Resources/lang', 'lvtags');
     }
 
     private function handleViews() {
 
-        $this->loadViewsFrom(__DIR__.'/../views', 'lvtags');
+        $this->loadViewsFrom(__DIR__.'/Resources/views', 'lvtags');
 
-        $this->publishes([__DIR__.'/../views' => base_path('resources/views/vendor/lvtags')]);
+        $this->publishes([__DIR__.'/Resources/views' => base_path('resources/views/vendor/lvtags')]);
     }
 
     private function handleMigrations() {
 
-        $this->publishes([__DIR__ . '/../migrations' => base_path('database/migrations')]);
+        $this->publishes([__DIR__ . '/Database/Migrations' => base_path('database/migrations')]);
     }
 
     private function handleRoutes() {
 
-        include __DIR__.'/../routes.php';
+        include __DIR__.'/Routes/web.php';
+        include __DIR__.'/Routes/api.php';
+
     }
 }
